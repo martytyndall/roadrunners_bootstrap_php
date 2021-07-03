0b59770ea0a0b5669b34801df81ba15f7b4567ed
Marty Tyndall <m.tyndall1979@gmail.com>
2021-07-03 22:14:22 +0100

    03/07/2021 22.14

--- scripts.js
+++ scripts.js
@@ -24,8 +24,22 @@ function showSlides() {
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
-function showYear(){
-  var theDate = new Date()
-  theDate.getFullYear
- 
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
